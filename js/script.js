6aa7fba16f9cf433a4ba6917085514ad6abade74
Виталий <122453878+VitalySigaev@users.noreply.github.com>
2023-07-19 13:51:17 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+"use strict";
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        arrows: false,
+      
+    });
+
+})
